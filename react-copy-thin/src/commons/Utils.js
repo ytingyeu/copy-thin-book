247e9da82b9f47a2f5cd76972ec7a91c3732bbda
Tingyeu Yang <ytingyeu@asu.edu>
2019-10-18 13:33:16 -0700

    init

--- react-copy-thin/src/commons/Utils.js
+++ react-copy-thin/src/commons/Utils.js
@@ -4,8 +4,27 @@ export function getCurrentTab(callback) {
         active: true,
         currentWindow: true
     },
-    (tabs) => {
-        callback(tabs[0]);
-    });
+        (tabs) => {
+            callback(tabs[0]);
+        });
 }
+
+
+export function copyToClipboard(str) {
+    //bg.console.log('copyToClipboard');
+    //bg.console.log(str);
+
+    // create a new element and append it to DOM
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+
+    // select and execute copy
+    el.select();
+    document.execCommand('copy');
+
+    // remove useless element
+    document.body.removeChild(el);
+}
+
 
