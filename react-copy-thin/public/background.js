3e1c6b83aec4d9e5007624cc57a83179a83ec369
Tingyeu Yang <ytingyeu@asu.edu>
2023-02-01 02:44:02 -0800

    add message listener

--- react-copy-thin/public/background.js
+++ react-copy-thin/public/background.js
@@ -1,35 +1,41 @@
 /* global chrome */
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                // only enable extension under an item page
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        // Toranoana all age
-                        pageUrl: {
-                            hostContains: "ecs.toranoana.jp",
-                            pathContains: "tora/ec/item",
-                        },
-                    }),
-                    new chrome.declarativeContent.PageStateMatcher({
-                        // Toranoana R18
-                        pageUrl: {
-                            hostContains: "ec.toranoana.jp",
-                            pathContains: "tora_r/ec/item",
-                        },
-                    }),
-                    new chrome.declarativeContent.PageStateMatcher({
-                        // Melonbooks
-                        pageUrl: {
-                            hostContains: ".melonbooks.co.jp",
-                            queryContains: "product_id=",
-                        },
-                    }),
-                ],
-                actions: [new chrome.declarativeContent.ShowAction()],
+// TODO: Inspect View always turns to inactive after the extension is closed.
+// Maybe a bug, maybe not https://bugs.chromium.org/p/chromium/issues/detail?id=1271154
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // only enable extension under an item page
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            // Toranoana all age
+            pageUrl: {
+              hostContains: "ecs.toranoana.jp",
+              pathContains: "tora/ec/item",
             },
-        ]);
-    });
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            // Toranoana R18
+            pageUrl: {
+              hostContains: "ec.toranoana.jp",
+              pathContains: "tora_r/ec/item",
+            },
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            // Melonbooks
+            pageUrl: {
+              hostContains: ".melonbooks.co.jp",
+              queryContains: "product_id=",
+            },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()],
+      },
+    ]);
+  });
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+  console.log(message);
 });
