b1a3ff869e4c0a83bf100955f8745cfe65324c8c
Tingyeu Yang <ytingyeu@asu.edu>
2023-02-08 22:56:37 -0800

    Updated query func for melonbooks

--- react-copy-thin/src/commons/QueryFuncs.js
+++ react-copy-thin/src/commons/QueryFuncs.js
@@ -20,25 +20,23 @@ const queryFuncDict = {
     queryGenre: () =>
       document.querySelector(".js-product-detail-spec-genre").innerText,
   },
-  // TODO: update query string for melonbooks
+
   melonbooks: {
-    queryTitle: () =>
-      document.querySelectorAll("table.stripe tr.odd td")[0].innerText,
+    queryTitle: () => document.querySelector(".page-header").innerText,
 
     queryAuthor: () =>
-      document.querySelectorAll("table.stripe tr.odd td a")[2].innerText,
+      document.querySelectorAll(".product_info a")[2].innerText,
 
     queryCircle: () =>
-      document.querySelectorAll("table.stripe tr.odd td a")[0].innerHTML,
+      document.querySelectorAll(".product_info a")[0].innerText,
 
-    queryPrice: () => document.querySelector(".price").innerText,
+    queryPrice: () => document.querySelector(".yen.__discount").innerText,
 
     queryGenre: () =>
-      document.querySelectorAll("table.stripe tr.odd td")[3].innerText,
+      document.querySelectorAll(".table-wrapper th+td")[2].innerText,
   },
 };
 
-// TODO: replace tabs.executeScript to scripting.executeScript
 export function createQueryPromises(tab, shopName) {
   /*=========== create promises for different fields ===========*/
 
@@ -49,7 +47,7 @@ export function createQueryPromises(tab, shopName) {
         func: queryFuncDict[shopName].queryTitle,
       },
       function (results) {
-        console.log(results);
+        //console.log(results);
 
         if (results[0] == null) {
           resolve("title_not_found");
@@ -67,7 +65,7 @@ export function createQueryPromises(tab, shopName) {
         func: queryFuncDict[shopName].queryAuthor,
       },
       function (results) {
-        console.log(results);
+        //console.log(results);
 
         if (results[0] == null) {
           resolve("author_not_found");
@@ -85,7 +83,7 @@ export function createQueryPromises(tab, shopName) {
         func: queryFuncDict[shopName].queryCircle,
       },
       function (results) {
-        console.log(results);
+        //console.log(results);
 
         if (results[0] == null) {
           resolve("circle_not_found");
@@ -103,7 +101,7 @@ export function createQueryPromises(tab, shopName) {
         func: queryFuncDict[shopName].queryPrice,
       },
       function (results) {
-        console.log(results);
+        //console.log(results);
 
         if (results[0] == null) {
           resolve("price_not_found");
@@ -121,7 +119,7 @@ export function createQueryPromises(tab, shopName) {
         func: queryFuncDict[shopName].queryGenre,
       },
       function (results) {
-        console.log(results);
+        //console.log(results);
 
         if (results[0] == null) {
           resolve("genre_not_found");
